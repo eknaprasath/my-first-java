8c99455781c1f321afd395631d77bb5b72a35a3b
eknaprasath <supremeprasath@gmail.com>
2021-06-24 20:07:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
      public static void main(String[] args) {
 	 //This is simple HelloWorld 
-	    System.out.println("Hello, World March Webhook Trigger");
+	    System.out.println("Hello, World Jenkins");
 		
 	 }
 }
