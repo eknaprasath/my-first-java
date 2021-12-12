cfcbef927b963db2e73e2b17f280c044bfe943a7
eknaprasath <supremeprasath@gmail.com>
2021-12-12 12:11:00 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
      public static void main(String[] args) {
 	 //This is simple HelloWorld 
-	    System.out.println("Hello, World poll scm");
+	    System.out.println("Hello, World trigger test");
 		
 	 }
 }
