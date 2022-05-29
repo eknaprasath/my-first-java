026f253136ae60938dd876b7ab78fea08af9f7cb
eknaprasath <supremeprasath@gmail.com>
2022-05-29 20:20:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
      public static void main(String[] args) {
 	 //This is simple HelloWorld 
-	    System.out.println("Hello, World Web Hook");
+	    System.out.println("Hello, World javac test");
 		
 	 }
 }
